297c40a63469c1b195620b655d070221b56f0bec
GabrielFelix23 <srgfelix123@gmail.com>
2021-03-01 12:19:21 -0300

    Search Engine Optimisation (SEO)

--- next-site/pages/post/hello-there.tsx
+++ next-site/pages/post/hello-there.tsx
@@ -1,7 +1,13 @@
+import Head from 'next/head'
 import Image from 'next/image'
 
 const Hello = () => (
     <article>
+        <Head>
+            <title>Hello there :: My Blog</title>
+            <meta property="og:title" content="Hello there :: My Blog"/>
+        </Head>
+
         <h1>Hello There!</h1>
         <Image
             src="/post/hello-there/img1.jpg"
