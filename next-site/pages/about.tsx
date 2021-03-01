2bf000f702fc4209c7d6ef9ce2aa2964099b4c7f
GabrielFelix23 <srgfelix123@gmail.com>
2021-03-01 11:07:14 -0300

    Criando rotas estáticas: About page

---
+++ next-site/pages/about.tsx
@@ -0,0 +1,12 @@
+import Navigation from '../components/navigation'
+
+export default function About(){
+    return (
+        <>
+            <Navigation/>
+            <header>
+                <h2>Hello from abount page!</h2>
+            </header>
+        </>
+    )
+}
